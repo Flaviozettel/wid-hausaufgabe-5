09b72b197a3f1967bbc53e4c4bea2095cd22609c
Flaviozettel <flavio.zettel@students.fhnw.ch>
2024-10-24 00:10:25 +0200

    Erstellung MUI-Komponenten in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+import { BasicTable } from "./Tabelle";
+import { Titelleiste } from "./Titelleiste";
+import data from "./data/tableData.json";
+
 function App() {
+  const [auswahlService2, setAuswahlService2] = useState("");
+  const [auswahlAnbieter2, setAuswahlAnbieter2] = useState("");
+  const [suchtext2, setSuchtext2] = useState("");
   return (
-    <>
-      <h1>hello React</h1>
-    </>
+    <div className="App">
+      <div className="titelseite-container">
+        <Titelleiste
+          onChangeService2={setAuswahlService2}
+          onChangeAnbieter2={setAuswahlAnbieter2}
+          onChangeSuchfenster2={setSuchtext2}
+        />
+        <br />
+
+        <BasicTable
+          auswahlService2={auswahlService2}
+          auswahlAnbieter2={auswahlAnbieter2}
+          suchtext2={suchtext2}
+          data={data}
+        />
+      </div>
+    </div>
   );
 }
 
