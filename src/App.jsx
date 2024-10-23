4a054af1f67eae570635dc0ee4bcc041b9f58ea4
Flaviozettel <flavio.zettel@students.fhnw.ch>
2024-10-24 00:16:37 +0200

    Erstellung MUI-Komponenten in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 function App() {
-  <>
-    <Button>Click me</Button>
-    <TextField
-      id="filled-search"
-      label="Suchfeld Zusammenfassung"
-      type="search"
-      variant="filled"
-      sx={{
-        m: 0,
-        mt: -1,
-        width: 300,
-        height: 50,
-      }}
-    />
-  </>;
+  return (
+    <>
+      <Button>Click me</Button>
+      <TextField
+        id="filled-search"
+        label="Suchfeld Zusammenfassung"
+        type="search"
+        variant="filled"
+        sx={{
+          m: 0,
+          mt: -1,
+          width: 300,
+          height: 50,
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
